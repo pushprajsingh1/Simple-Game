eb03a27ec90f7260d0bbe4ec4d7b38f4aaf64c3d
Pushpraj Singh <rajsingh13730@gmail.com>
2021-07-19 16:50:54 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,43 @@
+import random
+
+# function for chosing the Sake , water or gun: 
+def gameWin(comp,you):
+    if comp == you:
+        return None
+    elif comp == 's':
+        if you == 'w':
+            return False
+        elif you == 'g':
+            return True
+    elif comp == 'w':
+        if you == 'g':
+            return False
+        elif you == 's':
+            return True
+    elif comp == 'g':
+        if you == 's':
+            return False
+        elif you == 'w':
+            return True 
+        
+print("Comp Turn: Snake(s) Water(w) or Gun(g)")
+randNo = random.randint(1, 3)     # it is for thr selction of computer as a ramdom variable
+if randNo == 1:
+    comp = 's'
+elif randNo == 2:
+    comp = 'w'
+elif randNo == 3:
+    comp = 'g'
+    
+you = input("Your Turn : Snake(s) Water(w) or Gun(g)/") # taking user input 
+a = gameWin(comp, you)
+
+print(f"Computer chose {comp}") 
+print(f"You chose {you}")
+
+if a == None:
+    print("The game is tie !")
+elif a:
+    print("You Win!")
+else:
+    print("You Lose")
